49fed960cba9bff1cbd12ef5ad45dc9e5cd8aaef
JisuEun <zkxktmxmfhvl@gmail.com>
2024-03-29 00:47:13 +0900

    express-mysql-session

--- server.js
+++ server.js
@@ -1,10 +1,39 @@
 const express = require('express')
 const app = express()
+const session = require('express-session')
+const MySQLStore = require('express-mysql-session')(session)
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
+
+const options = {
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: '0000',
+    database: 'story_flow_analysis_text_editor'
+}
+
+const sessionStore = new MySQLStore(options)
+
+app.use(session({ // 세션이 시작됨, 객체 전달
+    name: 'session_cookie_name',
+    secret: 'session_cookie_secret', // Required option(필수)
+    resave: false, // 그냥 false
+    saveUninitialized: true, // 세션이 필요한 경우에만 구동됨 (false로 하면 서버에 부담 줌)
+    store: sessionStore
+}))
+
+
+
+app.get('/', function (req, res, next) {
+    console.log(req.session) // 객체 추가
+    if(req.session.num === undefined) { // 저장이 기본적으로는 메모리에 됨 -> 휘발성임
+        req.session.num = 1;
+    } else {
+        req.session.num = req.session.num + 1
+    }
+    res.send(`Views : ${req.session.num} `)
 })
 
-app.listen(3000, () =>  {
-    console.log('server listening at http://localhost:3000')
-})
+app.listen(3000, function(){
+    console.log('3000!');
+});
