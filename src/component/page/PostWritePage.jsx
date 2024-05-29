82075a973e1f20abba0af5b2f0c42d669c48131a
Sumin Kim <75007784+Sumin549@users.noreply.github.com>
2024-05-29 14:16:58 +0900

    add summary title

--- src/component/page/PostWritePage.jsx
+++ src/component/page/PostWritePage.jsx
@@ -425,9 +425,8 @@ function PostWritePage() {
         );
 
         const summary = response.data.data; // 백엔드에서 반환된 요약 텍스트를 가져옵니다.
-        quill.insertText(0,'<요약 내용>', {
+        quill.insertText(0,'요약 내용', {
           color: '#0040FF',
-          bold: true,
         });
         quill.insertText(quill.getLength(), `\n${summary}\n`);
       } catch (error) {
