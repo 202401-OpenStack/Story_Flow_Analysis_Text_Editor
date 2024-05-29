bd2b4d5edee739fafe6c20e1e6602926f056215c
ahnl <ahnl1297@gmail.com>
2024-05-29 21:25:44 +0900

    testing analyzeCharacterCount 3

--- src/component/page/PostWritePage.jsx
+++ src/component/page/PostWritePage.jsx
@@ -337,9 +337,10 @@ function PostWritePage() {
           }
         );
 
-        const summary = JSON.parse(response.data.data); // 백엔드에서 반환된 요약 텍스트를 가져옵니다.
+        const summary = response.data.data; // 백엔드에서 반환된 요약 텍스트를 가져옵니다.
+        console.log(summary, typeof summary);
+        console.log(JSON.parse(summary), typeof JSON.parse(summary));
         const characterCount = summary.length;
-        console.log(summary, characterCount);
         const text = `이 글의 등장인물은 ${summary}(으)로 총 ${characterCount}명입니다.`;
         quill.insertText(quill.getLength(), text);
         quill.setSelection(quill.getLength(), 0);
