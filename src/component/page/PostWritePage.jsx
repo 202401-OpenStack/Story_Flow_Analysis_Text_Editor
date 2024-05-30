0d425cf4b8956a165811723f64b9b8dc602b5d71
Sumin Kim <75007784+Sumin549@users.noreply.github.com>
2024-05-30 20:50:46 +0900

    add analysis title

--- src/component/page/PostWritePage.jsx
+++ src/component/page/PostWritePage.jsx
@@ -282,7 +282,10 @@ function PostWritePage() {
         );
 
         const summary = response.data.data; // 백엔드에서 반환된 요약 텍스트를 가져옵니다.
-        quill.insertText(quill.getLength(), `\n${summary}\n`);
+        quill.insertText(quill.getLength(), "\n<주제>");
+        quill.insertText(quill.getLength(), `\n${summary}\n`, {
+          color: "#0040FF",
+        });
         quill.setSelection(quill.getLength(), 0);
       } catch (error) {
         if (error.response) {
@@ -310,7 +313,10 @@ function PostWritePage() {
         );
 
         const summary = response.data.data; // 백엔드에서 반환된 요약 텍스트를 가져옵니다.
-        quill.insertText(quill.getLength(), `\n${summary}\n`);
+        quill.insertText(quill.getLength(), "\n<키워드>");
+        quill.insertText(quill.getLength(), `\n${summary}\n`, {
+          color: "#0040FF",
+        });
         quill.setSelection(quill.getLength(), 0);
       } catch (error) {
         if (error.response) {
@@ -342,7 +348,10 @@ function PostWritePage() {
         console.log(characterList, typeof characterList);
         const characterCount = characterList.length;
         const text = `이 글의 등장인물은 ${characterList}(으)로 총 ${characterCount}명입니다.`;
-        quill.insertText(quill.getLength(), text);
+        quill.insertText(quill.getLength(), "\n<인물 수>");
+        quill.insertText(quill.getLength(), text, {
+          color: "#0040FF",
+        });
         quill.setSelection(quill.getLength(), 0);
       } catch (error) {
         if (error.response) {
@@ -432,7 +441,10 @@ function PostWritePage() {
         );
 
         const summary = response.data.data; // 백엔드에서 반환된 요약 텍스트를 가져옵니다.
-        quill.insertText(quill.getLength(), `\n${summary}\n`);
+        quill.insertText(quill.getLength(), "\n<이야기 흐름>");
+        quill.insertText(quill.getLength(), text, {
+          color: "#0040FF",
+        });
         quill.setSelection(quill.getLength(), 0);
       } catch (error) {
         if (error.response) {
