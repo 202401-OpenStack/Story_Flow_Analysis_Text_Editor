8ce567979bf8439e2d2a9e377534384ac711cfa5
Sumin Kim <75007784+Sumin549@users.noreply.github.com>
2024-05-30 21:36:56 +0900

    edit graph design

--- src/component/page/PostWritePage.jsx
+++ src/component/page/PostWritePage.jsx
@@ -693,12 +693,12 @@ function PostWritePage() {
                 nodeAutoColorBy="group"
                 nodeCanvasObject={(node, ctx, globalScale) => {
                   const label = node.name;
-                  const fontSize = 8;
+                  const fontSize = 6;
                   ctx.fillStyle = node.color;
                   ctx.beginPath();
-                  ctx.arc(node.x, node.y, 8, 0, 2 * Math.PI, false);
+                  ctx.arc(node.x, node.y, 6, 0, 2 * Math.PI, false);
                   ctx.fill();
-                  ctx.font = "${fontSize}px Arial";
+                  ctx.font = "6px Arial";
                   ctx.textAlign = "center";
                   ctx.textBaseline = "middle";
                   ctx.fillStyle = "white";
@@ -719,7 +719,7 @@ function PostWritePage() {
                   // 텍스트 라벨의 위치를 조정
                   const offset = link.source.id < link.target.id ? -5 : 5;
 
-                  ctx.font = "8px Arial";
+                  ctx.font = "4px Arial";
                   ctx.fillStyle = "black";
                   ctx.fillText(
                     link.relationship,
